9873a21270609c01bdb9842bd773b9d894b929a0
Nikita <121577151+NikitaSizPl@users.noreply.github.com>
2024-07-21 23:50:50 +0200

    upgrade file main.py -> add raise HTTPException

--- sql_app/main.py
+++ sql_app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from sql_app.database import SessionLocal, engine
 from sqlalchemy.orm import Session
 from sql_app import product_model, crud
@@ -25,16 +25,22 @@ async def ten_itims(db: Session = Depends(get_db)):
 @app.get("/category/", tags=['all_category'])
 async def get_all_category(db: Session = Depends(get_db)):
     category = crud.get_all_category(db)
+    if category is None:
+        raise HTTPException(status_code=400, detail="Category not found ERROR")
     return category
 
 
 @app.get("/category/{cat_id}/items/", tags=['all_in_one_category'])
 async def read_one_by_category(cat_id: int, db: Session = Depends(get_db)):
     only_one_category = crud.get_one_by_category(cat_id, db)
+    if only_one_category is None:
+        raise HTTPException(status_code=400, detail="Category not found ERROR")
     return only_one_category
 
 
 @app.get("/category/{cat_id}/items/{item_id}", tags=['one_in_one_category'])
 async def read_product_by_id(cat_id: int, item_id: int, db: Session = Depends(get_db)):
     only_one_item_id = crud.get_product_by_id(cat_id, item_id, db)
+    if only_one_item_id is None:
+        raise HTTPException(status_code=400, detail="Item not found in Category ERROR")
     return only_one_item_id
