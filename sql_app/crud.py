17b7466c36cee1c9856a41403bd6b9f5503b7b3c
Nikita <121577151+NikitaSizPl@users.noreply.github.com>
2024-07-21 12:56:12 +0200

    add update file crud.py -> upgrade def get_product_by_id

--- sql_app/crud.py
+++ sql_app/crud.py
@@ -12,4 +12,7 @@ def get_one_by_category(cat_id: int, db: Session):
 
 
 def get_product_by_id(cat_id: int, item_id: int, db: Session):
-    return db.query(Item).filter(Category.id == cat_id, Item.id == item_id).first()
+    category = db.query(Category).filter(Category.id == cat_id).first()
+    if category:
+        return db.query(Item).filter(Item.id == item_id, Item.category_id == cat_id).first()
+    return None  # Return None if the category does not exist
