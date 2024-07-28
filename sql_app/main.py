6348e56c493fef61319d10f1f7948c7d94d8c68b
Nikita <121577151+NikitaSizPl@users.noreply.github.com>
2024-07-28 13:42:48 +0200

    upgrade main.py

--- sql_app/main.py
+++ sql_app/main.py
@@ -30,25 +30,28 @@ async def ten_itims(request: Request):
     return templates.TemplateResponse("home.html", {"request": request, "name": "Home"})
 
 
-@app.get("/category/", tags=['all_category'], response_class=HTMLResponse)
+@app.get("/category", tags=['all_category'], response_class=HTMLResponse)
 async def get_all_category(request: Request, db: Session = Depends(get_db)):
     category = crud.get_all_category(db)
     if category is None:
         raise HTTPException(status_code=400, detail="Category not found ERROR")
-    return templates.TemplateResponse("assort.html", {"request": request, "name": "category", "category": category})
+    return templates.TemplateResponse(
+        "all_categor.html", {"request": request, "name": "category", "category": category})
 
 
-@app.get("/category/{cat_id}/items/", tags=['all_in_one_category'])
-async def read_one_by_category(cat_id: int, db: Session = Depends(get_db)):
+@app.get("/category/{cat_id}/items", tags=['all_in_one_category'], response_class=HTMLResponse)
+async def read_one_by_category(request: Request, cat_id: int, db: Session = Depends(get_db)):
     only_one_category = crud.get_one_by_category(cat_id, db)
     if only_one_category is None:
         raise HTTPException(status_code=400, detail="Category not found ERROR")
-    return only_one_category
+    return templates.TemplateResponse(
+        "one_categor.html", {"request": request, "name": "only_one_category", "only_one_category": only_one_category})
 
 
-@app.get("/category/{cat_id}/items/{item_id}", tags=['one_in_one_category'])
-async def read_product_by_id(cat_id: int, item_id: int, db: Session = Depends(get_db)):
+@app.get("/category/{cat_id}/items/{item_id}", tags=['one_in_one_category'], response_class=HTMLResponse)
+async def read_product_by_id(request: Request, cat_id: int, item_id: int, db: Session = Depends(get_db)):
     only_one_item_id = crud.get_product_by_id(cat_id, item_id, db)
     if only_one_item_id is None:
         raise HTTPException(status_code=400, detail="Item not found in Category ERROR")
-    return only_one_item_id
+    return templates.TemplateResponse(
+        "item.html", {"request": request, "name": "item", "only_one_item_id": only_one_item_id})
