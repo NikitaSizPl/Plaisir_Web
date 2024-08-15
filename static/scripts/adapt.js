0963561f229267352717de43d20543d5b4c42acf
Nikita <121577151+NikitaSizPl@users.noreply.github.com>
2024-08-15 15:23:41 +0200

    add click -> active to burger languages

--- static/scripts/adapt.js
+++ static/scripts/adapt.js
@@ -5,3 +5,9 @@ burger.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
 
+const dokglobal = document.getElementById('doker-global');
+const dokspan = document.getElementById('doker-span');
+
+dokglobal.addEventListener('click', () => {
+    dokspan.classList.toggle('active');
+});
