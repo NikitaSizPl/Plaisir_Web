5d7d96e200dc97dfcd420101b6fff9cae6470236
Nikita <121577151+NikitaSizPl@users.noreply.github.com>
2024-08-17 09:57:20 +0200

    upgrade class Category -> add name_rus + name_eng

--- sql_app/product_model.py
+++ sql_app/product_model.py
@@ -10,6 +10,10 @@ class Category(Base):
     name = Column(String, nullable=False)
     images_id = Column(Integer)
     images_url = Column(String)
+    # Ссылка на табулицу на русском
+    name_rus = Column(String)
+    # Ссылка на табулицу на англиском
+    name_eng = Column(String)
     #  Зависимость таблицы и название "category"
     items = relationship("Item", back_populates="category")
 
@@ -25,8 +29,15 @@ class Item(Base):
     images_id = Column(Integer)
     images_url = Column(String)
     innader_info = Column(String)
+    # Ссылка на название и описание на русском
+    name_rus = Column(String)
+    description_rus = Column(String)
+    innader_info_rus = Column(String)
+    # Ссылка на название и описание на англиском
+    name_eng = Column(String)
+    description_eng = Column(String)
+    innader_info_eng = Column(String)
 
-    # сделать имя категории
     #  Зависимость таблицы к классу "Product"
     category_id = Column(Integer, ForeignKey('categories.id'))
     category = relationship("Category", back_populates="items")
