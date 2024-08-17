bcf59a9c8e1588ddc137215cf636e96faf848f0c
Nikita <121577151+NikitaSizPl@users.noreply.github.com>
2024-08-17 16:20:34 +0200

    upgrade adapt.js  document.addEventListener('click', () => {         dokspan.classList.remove('active');         });

--- static/scripts/adapt.js
+++ static/scripts/adapt.js
@@ -1,13 +1,32 @@
-const burger = document.getElementById('burger');
-const navLinks = document.getElementById('nav-links');
+document.addEventListener('DOMContentLoaded', () => {
+    const burger = document.getElementById('burger');
+    const navLinks = document.getElementById('nav-links');
 
-burger.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
+// Открытие/ закрытие по нажатию на меню
+    burger.addEventListener('click', (event) => {
+        navLinks.classList.toggle('active');
+        event.stopPropagation();
+        });
+
+// Закрытие по нажатию вне меню
+    document.addEventListener('click', () => {
+        navLinks.classList.remove('active');
+        });
 });
 
-const dokglobal = document.getElementById('doker-global');
-const dokspan = document.getElementById('doker-span');
 
-dokglobal.addEventListener('click', () => {
-    dokspan.classList.toggle('active');
+document.addEventListener('DOMContentLoaded', () => {
+    const dokglobal = document.getElementById('doker-global');
+    const dokspan = document.getElementById('doker-span');
+
+// Открытие/ закрытие по нажатию на меню
+    dokglobal.addEventListener('click', (event) => {
+        dokspan.classList.toggle('active');
+        event.stopPropagation();
+    });
+
+// Закрытие по нажатию вне меню
+    document.addEventListener('click', () => {
+        dokspan.classList.remove('active');
+        });
 });
